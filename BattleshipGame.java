807d9b47d0d49f87d3bce26ac8d9d8cbde46f898
Zijianx <92890653+Zijianx@users.noreply.github.com>
2021-12-12 22:35:18 -0500

    Add files via upload
    BattleshipGame second commit

--- BattleshipGame.java
+++ BattleshipGame.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BattleshipGame {
@@ -16,10 +17,37 @@ public class BattleshipGame {
 
 	private int[] getInput() {
 		Scanner scnr = new Scanner(System.in);
-		System.out.println("Input X coordinate:");
-		int x = scnr.nextInt();
-		System.out.println("Input Y coordinate:");
-		int y = scnr.nextInt();
+		int x;
+		int y;
+		while (true) {
+			System.out.println("Input X coordinate (0 - 9):");
+			try {
+				x = scnr.nextInt();
+				if (this.isValidInput(x)) {
+					break;
+				} else {
+					System.out.println("Not valid number, please input again.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Not valid number, please input again.");
+				this.getInput();
+			}
+		}
+
+		while (true) {
+			System.out.println("Input Y coordinate (0 - 9):");
+			try {
+				y = scnr.nextInt();
+				if (this.isValidInput(y)) {
+					break;
+				} else {
+					System.out.println("Not valid number, please input again.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Not valid number, please input again.");
+				this.getInput();
+			}
+		}
 		int[] result = { x, y };
 		return result;
 	}
